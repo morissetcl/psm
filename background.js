0e0e46c133bb7ef0a0d0fabd190a38a554b3298c
Clement Morisset <morissetcl87@gmail.com>
2021-01-09 11:20:43 -0500

    Fix bug closing tab

--- background.js
+++ background.js
@@ -9,29 +9,16 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 });
 
 function accomodationUrls(urls) {
-  // const url = urls[0][0]
-  // chrome.tabs.create({ url: url, active: false }, function(tab){ getSurfaceArea(tab.id)});
-
-  // Object.values(urls[0]).forEach(function(url) {
-  //   if (typeof url === 'string') {
-  //     (function () {
-  //       setTimeout(function () {
-  //         chrome.tabs.create({ url: url, active: false }, function(tab){ getSurfaceArea(tab.id)});
-  //       }, 1000);
-  //     })();
-  //   }
-  // });
-
   var urls = Object.values(urls[0]);
   for (var i = 0; i < urls.length; i++) {
-      (function (i) {
-          setTimeout(function () {
-            console.log(urls[i])
-            chrome.tabs.create({ url: urls[i], active: false }, function(tab){ getSurfaceArea(tab.id)});
-          }, 3000 * i);
-      })(i);
+    (function (i) {
+        setTimeout(function () {
+          console.log(urls[i])
+          chrome.tabs.create({ url: urls[i], active: false }, function(tab){ getSurfaceArea(tab.id)});
+        }, 3000 * i);
+    })(i);
   };
-}
+};
 
 function getSurfaceArea(tabId) {
   chrome.tabs.onUpdated.addListener(function (tabId , info) {
@@ -40,16 +27,20 @@ function getSurfaceArea(tabId) {
 }
 
 function test(data) {
-  if(data[0] != null) {
-    if(data[0][0] != "") {
-      chrome.tabs.executeScript(data[0][1], { code: `[$(".psm:first").text(), $(".sharethis").attr("data-mlsnumber")]` }, okok);
-      chrome.tabs.remove(data[0][1], function() { });
-    }
-  }
-}
+  chrome.tabs.query({ active: true }, function (tabs) {
+    if(data[0][1] !== tabs[0].id) {
+      if(data[0] != null) {
+        if(data[0][0] != "") {
+          chrome.tabs.executeScript(data[0][1], { code: `[$(".psm:first").text(), $(".sharethis").attr("data-mlsnumber")]` }, okok);
+          chrome.tabs.remove(data[0][1], function() { });
+        };
+      };
+    };
+  });
+};
 
 function okok(data) {
   chrome.tabs.query({ active: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { data: data });
   });
-}
+};
